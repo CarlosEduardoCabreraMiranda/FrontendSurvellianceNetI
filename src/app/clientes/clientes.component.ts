31ba7b42a5075403efd8c51ebd906e0a0cf01f83
JUAN JOSE URBANO PERDOMO <131488381+JuanJoseUrbano@users.noreply.github.com>
2023-09-19 08:45:14 -0500

    Update clientes.component.ts
    Agregando más clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -8,8 +8,18 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent {
 clientes: Cliente[] = [
-  {idCliente:1, nombre:"Carlos", apellido:"Cabrera", email:"example@gmail.com", FechaCreado:"Marzo"},
-  {idCliente:2, nombre:"Juan Jose", apellido:"Urbano", email:"example2@gmail.com", FechaCreado:"Abril"}
+  {idCliente:1, nombre:'Julian', apellido:'Quimbayo',
+  email:'jaquimbayoc7@gmail.com',fechaCreado:'2023-09-18'},
+  {idCliente:2, nombre:'Juan José', apellido:'Urbano Perdomo',
+  email:'urbanoperdomoj@gmail.com',fechaCreado:'2023-09-18'},
+  {idCliente:3, nombre:'Andres', apellido:'Castro',
+  email:'andres7@gmail.com',fechaCreado:'2023-09-18'},
+  {idCliente:4, nombre:'Carlos Eduardo', apellido:'Cabrera Miranda',
+  email:'ccabrera@gmail.com',fechaCreado:'2023-09-18'},
+  {idCliente:5, nombre:'Cindy', apellido:'Vargas Duque',
+  email:'cindyv@gmail.com',fechaCreado:'2023-09-18'},
+  {idCliente:6, nombre:'Camilo', apellido:'Chavarro Guenis Bailador',
+  email:'chavarroguenis@gmail.com',fechaCreado:'2023-09-18'}
 ];
 
 }
